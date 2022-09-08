6d5ffac21d5d1dd41beada566a3790c0b8cd16c1
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-08 12:53:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 
 #!/bin/bash
 /**
-*main - print the string in the put function
+* main -print the string in the put function
 *
-*Description:using the main function
-*this programm prints 'Programming is like building a multilingual puzzle
-*Return: 0
+* Description: using the main function
+* this program prints 'Programming is like building a multilingual puzzle'
+* Return: 0
 */
 int main(void)
-{ 
-   puts("\"programming is like building a multilingual");
-   return (0)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return (0);
 }
