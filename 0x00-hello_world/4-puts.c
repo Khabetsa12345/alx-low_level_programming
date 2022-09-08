8594e5fc2f1623812aa4e841304a225116a67e70
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-08 12:03:09 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+/**
+*main - print the string in the put function
+*
+*Description:using the main function
+*this programm prints 'Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{ 
+   puts("\"programming is like building a multilingual");
+   return (0)
+}
