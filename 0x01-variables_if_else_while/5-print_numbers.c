069f0ce3cf9cbd24d8d8f9187577c4c1aa469a60
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-12 09:16:17 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,8 +3,9 @@
 
 /**
  * main -Entry point
- * Description: prints the numbers from 0 to 9
- * Rerurn: Always (0)
+ * Description: prints all single digit numbers 
+ * base 10 starting from 0
+ * Return: Always (0)
  */
 int main(void)
 {
@@ -18,5 +19,4 @@ int main(void)
 
 	return (0);
 }
-
 
