1107c16b31810e790258eb0b3f9e66e2a3936d8a
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-13 13:03:29 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _lowercase - to check for the lowercase character
+ * _islower - to check for the lowercase character
  * @c: int thar will be used for the argument of the function
  * Return: 0
  */
