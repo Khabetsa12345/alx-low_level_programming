2dd7829e50cf910430924074fd51d71a02ad260f
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-14 14:39:11 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit = n % 10; 
 	if (last_digit < 0)
-	       last digit * = -1;
+		last_digit *= -1;
 
-	_putchar(last_digit + '0');
+	_putchar (last_digit + '0');
 
-	return (last_digit);
+	return(lastdigit);
 }
-
-	
+		
