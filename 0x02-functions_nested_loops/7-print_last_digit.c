d64c87137f314974054381498f8127835397274f
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-14 13:43:57 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,17 @@
 /**
  * print_last_digit - function prints the last digit of a number
  * @n: is the number in question
- *
- * Return: value of the last digit 
+ * Return: 0 
  */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	
 	if (last_digit < 0)
 	       last digit * = -1;
 
 	_putchar(last_digit + '0');
 
 	return (last_digit);
-}	
+}
+
 	
