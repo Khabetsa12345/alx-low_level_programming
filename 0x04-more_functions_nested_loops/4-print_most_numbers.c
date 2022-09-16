32579c52496dda26bb7fd7a8cabed444ef382149
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-16 05:24:26 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_most_numbers - prints the numbers with _putchar
+*
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if ((n == 50 || n == 52))
+		{
+			continue;
+		}
+		_putchar(n);
+
+		_putchar(10);
+	}
+}
