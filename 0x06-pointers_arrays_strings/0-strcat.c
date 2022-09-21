c136a27c27078aa98503f549f58758b678ba9e1a
Christabel Khakali <khabetsachristabel@gmail.com>
2022-09-21 12:13:44 -0700

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _strcat - concatenates two strings
+* _strcat - function that concatenates two strings
 * @dest: first parameter
 * dest: second parameter
-* Return: string
+* Return: dest
 */
 char *_strcat(char *dest, char *src)
 {
