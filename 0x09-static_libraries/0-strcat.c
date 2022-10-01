dd4da8df352b711d31c8f2b8ccbd4e8910d1d126
Christabel Khakali <khabetsachristabel@gmail.com>
2022-10-01 10:12:34 -0700

    static library libmy.a

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates @src to @dest
+* @src: the source string to append to @dest
+* @dest: the destiation string to be concatenated upon
+* Return: pointer to the resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
+
+
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - Returns the length of a string
+* @s: character type pointer
+*
+* Return: The length of @s
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
