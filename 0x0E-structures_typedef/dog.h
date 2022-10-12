6c2a7ab2c9e152a3cdc5e04b6afd1eec17b4ceb6
Christabel Khakali <khabetsachristabel@gmail.com>
2022-10-12 13:25:48 -0700

    a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * struct dog - Dog attributes
@@ -20,6 +20,7 @@ struct dog
 /**
 * dog_t - Typedef for dog structure
 */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
