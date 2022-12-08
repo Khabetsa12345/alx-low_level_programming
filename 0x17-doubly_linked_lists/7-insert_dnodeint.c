7ff3e12e166f9f720415593239ec9131ced71f87
Christabel Khakali <khabetsachristabel@gmail.com>
2022-12-08 12:48:08 -0800

    doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = tmp;
 	new->next = tmp->next;
-	mp->next->prev = new;
+	tmp->next->prev = new;
 	tmp->next = new;
 
 	return (new);
