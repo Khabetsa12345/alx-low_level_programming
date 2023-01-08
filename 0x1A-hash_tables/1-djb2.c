71a4a217c746236fad180fc9dd4e75ba47cb1d75
Christabel Khakali <khabetsachristabel@gmail.com>
2023-01-08 14:39:32 -0800

    hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,21 @@
-#!/usr/bin/python3
-"""
-Contains definiton for the class MyList that inherits from list.
-"""
+#include "hash_tables.h"
 
+/**
+* hash_djb2 - implementation of the djb2 algorithm
+* @str: string used to generate hash value
+*
+* Return: hash value
+*/
 
-class MyList(list):
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
 
-    """definiton for the class MyList that inherits from list.
-    """
-    def print_sorted(self):
-        """Prints list elements(int) in ascending order"""
-        sortedlist = sorted(self)
-        print(sortedlist)
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
